3ca211547d5f0be286ea9c48fd5267411036bb17
Jon Ong <jonongjs@gmail.com>
2014-05-11 18:16:07 +0800

    Added bullets and shooting.

--- blast.js
+++ blast.js
@@ -8,30 +8,37 @@ window.onload = function() {
 		UP: false,
 		DOWN: false,
 		LEFT: false,
-		RIGHT: false
+		RIGHT: false,
+		SHOOT: false
 	};
+	var PI_180 = Math.PI/180.0;
 	var INV_PI_180 = 180.0/Math.PI;
 
 	var player = { shape: null, vel: {x: 0, y: 0}, acc: {x: 0, y: 0}, maxspeed: 200, radius: 10 };
 	var enemies = [];
-	var bullets;
+	var bullets = [];
 	var background;
 
-	function init() {
-		var i;
+	var enemySpawner;
+	var bulletSpawner;
 
+	function init() {
 		background = new createjs.Shape();
 		var bgGfx = background.graphics;
 		bgGfx.setStrokeStyle(1).beginStroke("#505050");
 		var dx = gameWidth / divisions;
-		for (i=0; i<=divisions; ++i) {
+		for (var i=0; i<=divisions; ++i) {
 			var div = i*dx;
 			bgGfx.moveTo(div, 0).lineTo(div, gameHeight);
 			bgGfx.moveTo(0, div).lineTo(gameWidth, div);
 		}
 		stage.addChild(background);
 
-		createEnemyPool();
+		createEnemyPool(15);
+		createBulletPool(100);
+
+		enemySpawner = getSpawner(1.5, spawnEnemy);
+		bulletSpawner = getSpawner(0.2, spawnBullet);
 
 		player.shape = new createjs.Shape();
 		player.shape.graphics.setStrokeStyle(1, "round")
@@ -52,8 +59,7 @@ window.onload = function() {
 		resize();
 	}
 
-	function createEnemyPool() {
-		var count = 15;
+	function createEnemyPool(count) {
 		for (; count>0; --count) {
 			var enemy = { shape: null, vel: {x: 0, y: 0}, acc: {x: 0, y: 0}, maxspeed: 100, radius: 10 };
 			enemy.shape = new createjs.Shape();
@@ -63,6 +69,19 @@ window.onload = function() {
 			enemy.shape.visible = false;
 			enemies.push(enemy);
 			stage.addChild(enemy.shape);
+		}
+	}
+
+	function createBulletPool(count) {
+		for (; count>0; --count) {
+			var bullet = { shape: null, vel: {x: 0, y: 0}, acc: {x: 0, y: 0}, maxspeed: 1000, radius: 2 };
+			bullet.shape = new createjs.Shape();
+			bullet.shape.graphics.setStrokeStyle(1, "round")
+				.beginStroke("#cccccc")
+				.drawCircle(0, 0, bullet.radius);
+			bullet.shape.visible = false;
+			bullets.push(bullet);
+			stage.addChild(bullet.shape);
 		}
 	}
 
@@ -80,11 +99,13 @@ window.onload = function() {
 		// Game updates
 		var elapsed = createjs.Ticker.getInterval() * 0.001;
 
-		spawner.update(elapsed);
+		enemySpawner.update(elapsed);
+		bulletSpawner.update(elapsed);
 
 		updateMotion(player, elapsed);
 		collideWithWalls(player, false);
 		enemies.forEach(function(elt, i) { updateMotion(elt, elapsed); collideWithWalls(elt, true); });
+		bullets.forEach(function(elt, i) { updateMotion(elt, elapsed); checkBulletCollisions(elt); });
 		if (player.acc.y != 0 || player.acc.x != 0)
 			player.shape.rotation = Math.atan2(player.acc.y, player.acc.x) * INV_PI_180 - 90;
 
@@ -92,6 +113,9 @@ window.onload = function() {
 	}
 
 	function updateMotion(obj, elapsed) {
+		if (!obj.shape.visible)
+			return;
+
 		if (obj.acc.x == 0)
 			obj.vel.x *= 0.8;
 		else
@@ -134,43 +158,89 @@ window.onload = function() {
 		}
 	}
 
-	var spawner = {
-		spawnInterval: 2.0, // number of seconds to wait before spawning
-		spawnElapsed: 0.0,
+	function checkBulletCollisions(bullet) {
+		if (bullet.shape.visible) {
+			var x = bullet.shape.x;
+			var y = bullet.shape.y;
+			var radius = bullet.radius;
 
-		update: function(elapsed) {
-			this.spawnElapsed += elapsed;
-			if (this.spawnElapsed >= this.spawnInterval) {
-				this.spawnEnemy();
-				this.spawnElapsed -= this.spawnInterval;
-			}
-		},
-
-		freeFilter: function(elt, i) {
-			return !elt.shape.visible;
-		},
-
-		spawnEnemy: function() {
-			var freeEnemies = enemies.filter(this.freeFilter);
-			if (freeEnemies.length == 0)
+			if (x-radius <= 0 || x+radius >= gameWidth || y-radius <= 0 || y+radius >= gameHeight) {
+				bullet.shape.visible = false;
 				return;
-
-			var freeEnemy = freeEnemies[0];
-			var closeToPlayer = true;
-			while (closeToPlayer) {
-				freeEnemy.shape.x = Math.random() * (gameWidth-30)+15;
-				freeEnemy.shape.y = Math.random() * (gameHeight-30)+15;
-
-				closeToPlayer = sqDist(freeEnemy.shape, player.shape) < 25*25;
 			}
 
-			freeEnemy.shape.visible = true;
-			freeEnemy.vel.x = 0;
-			freeEnemy.vel.y = 0;
-			freeEnemy.acc.x = (Math.random()-0.5) * 20;
-			freeEnemy.acc.y = (Math.random()-0.5) * 20;
+			var hitlist = enemies.filter(function(enemy, i) {
+				var threshold = bullet.radius+enemy.radius;
+				return enemy.shape.visible && (sqDist(bullet.shape, enemy.shape) < threshold*threshold);
+			});
+			if (hitlist.length > 0) {
+				hitlist.forEach(hideObject);
+				bullet.shape.visible = false;
+			}
 		}
-	};
+	}
+
+	function getSpawner(interval, spawnFunc) {
+		var spawner = {
+			spawnInterval: interval, // number of seconds to wait before spawning
+			spawnElapsed: 0.0,
+
+			update: function(elapsed) {
+				this.spawnElapsed += elapsed;
+				if (this.spawnElapsed >= this.spawnInterval) {
+					this.spawnFunc();
+					this.spawnElapsed -= this.spawnInterval;
+				}
+			},
+
+			spawnFunc: spawnFunc
+		};
+		return spawner;
+	}
+
+	// Spawns an enemy if there are free enemies available
+	function spawnEnemy() {
+		var freeEnemies = enemies.filter(freeFilter);
+		if (freeEnemies.length == 0)
+			return;
+
+		var freeEnemy = freeEnemies[0];
+		var closeToPlayer = true;
+		while (closeToPlayer) {
+			freeEnemy.shape.x = Math.random() * (gameWidth-30)+15;
+			freeEnemy.shape.y = Math.random() * (gameHeight-30)+15;
+
+			closeToPlayer = sqDist(freeEnemy.shape, player.shape) < 25*25;
+		}
+
+		freeEnemy.shape.visible = true;
+		freeEnemy.vel.x = 0;
+		freeEnemy.vel.y = 0;
+		freeEnemy.acc.x = (Math.random()-0.5) * 20;
+		freeEnemy.acc.y = (Math.random()-0.5) * 20;
+	}
+
+	// Spawns a bullet if there are free bullets available
+	function spawnBullet() {
+		if (!keyStates["SHOOT"])
+			return;
+
+		var freeBullets = bullets.filter(freeFilter);
+		if (freeBullets.length == 0)
+			return;
+
+		var bullet = freeBullets[0];
+		var rotation = player.shape.rotation + 90;
+		var dir = { x: Math.cos(rotation*PI_180), y: Math.sin(rotation*PI_180) };
+		bullet.shape.x = player.shape.x + 12*dir.x;
+		bullet.shape.y = player.shape.y + 12*dir.y;
+
+		bullet.shape.visible = true;
+		bullet.vel.x = dir.x * 1000;
+		bullet.vel.y = dir.y * 1000;
+		bullet.acc.x = dir.x;
+		bullet.acc.y = dir.y;
+	}
 
 	// Helper functions
 	// Returns a keyUp/keyDown callback
@@ -185,6 +255,7 @@ window.onload = function() {
 		var KEYCODE_S = 83;
 		var KEYCODE_A = 65;
 		var KEYCODE_D = 68;
+		var KEYCODE_J = 74;
 
 		return function(evt) {
 			switch (evt.keyCode) {
@@ -203,6 +274,9 @@ window.onload = function() {
 				case KEYCODE_RIGHT:
 				case KEYCODE_D:
 					keyStates["RIGHT"] = state;
+					break;
+				case KEYCODE_J:
+					keyStates["SHOOT"] = state;
 					break;
 			}
 		};
@@ -223,6 +297,15 @@ window.onload = function() {
 		var dx = shape1.x - shape2.x;
 		var dy = shape1.y - shape2.y;
 		return dx*dx + dy*dy;
+	}
+
+	// Helper for getting free elements in a pool
+	function freeFilter(elt, i) {
+		return !elt.shape.visible;
+	}
+
+	function hideObject(elt, i) {
+		elt.shape.visible = false;
 	}
 
 	// Handles resizing of stage
